aab981e475f58b996c5fb2f5c0a825c7b70b035f
BGrant-git <bengranthome@gmail.com>
2021-07-12 15:59:21 +0100

    happy with work on contact page today

--- components/contactDetails/ContactDetails.js
+++ components/contactDetails/ContactDetails.js
@@ -1,21 +1,32 @@
-import { Container, InfoWrapper } from './contactDetailsStyles'
+import { CenterRule } from '../../store/context'
+import {
+	Container,
+	ContentWrapper,
+	DetailsWrapper,
+	ContactMethodWrapper,
+	LinkWrapper,
+} from './contactDetailsStyles'
 import { contactDetails } from '../../public/text-files/contactDetails'
 
 const ContactDetails = () => {
 	return (
 		<Container>
-			<InfoWrapper>
-				<h2>Telephone:</h2>
-				<h2>Email:</h2>
-			</InfoWrapper>
-			<div>
-				<a href="tel:0798765432">
-					<h2>{contactDetails.phoneNumber}</h2>
-				</a>
-				<a href="">
-					<h2>{contactDetails.email}</h2>
-				</a>
-			</div>
+			<h1>Or get in touch with Tom</h1>
+			<CenterRule style={{ background: 'black', width: '20px' }} />
+			<ContentWrapper>
+				<ContactMethodWrapper>
+					<h2>Telephone:</h2>
+					<h2>Email:</h2>
+				</ContactMethodWrapper>
+				<DetailsWrapper>
+					<a href="tel:0798765432">
+						<h2>{contactDetails.phoneNumber}</h2>
+					</a>
+					<a href="">
+						<h2>{contactDetails.email}</h2>
+					</a>
+				</DetailsWrapper>
+			</ContentWrapper>
 		</Container>
 	)
 }
--- pages/contact.js
+++ pages/contact.js
@@ -14,9 +14,11 @@ const backgroundImg = require('../public/images/homepage/naomi-hebert-MP0bgaS_d1
 const Contact = () => {
 	return (
 		<Container backgroundImg={backgroundImg}>
+			<StyledH1>Contact Us</StyledH1>
+			<CenterRule />
 			<ContentWrapper>
-				<ContactDetails />
 				<ContactForm />
+				<ContactDetails />
 			</ContentWrapper>
 		</Container>
 	)
