5cb820801f519458762f682f5a4538b1a47379fb
BGrant-git <bengranthome@gmail.com>
2021-07-27 11:44:25 +0100

    testing animate wrapper on hero comp

--- components/homeImageText/HomeImageText.js
+++ components/homeImageText/HomeImageText.js
@@ -1,7 +1,8 @@
 import Link from 'next/link'
 import { useContext } from 'react'
 import { Button } from '@material-ui/core'
-import { motion } from 'framer-motion'
+
+import AnimateWrapper from '../framerWrappers/AnimateWrapper'
 
 import { StoreContext } from '../../store/context'
 import { landingText } from '../../public/text-files/homepageText'
@@ -17,26 +18,22 @@ import {
 const HomeImageText = () => {
 	const { framerLeftSlideVariants } = useContext(StoreContext)
 	return (
-		// <motion.div
-		// 	initial="hidden"
-		// 	animate="visible"
-		// 	variants={framerLeftSlideVariants}
-		// >
-		<Container>
-			<TextWrapper>
-				<StyledH1>{landingText.heading}</StyledH1>
-				<StyledH2>{landingText.subHeading}</StyledH2>
-			</TextWrapper>
+		<AnimateWrapper variants={framerLeftSlideVariants}>
+			<Container>
+				<TextWrapper>
+					<StyledH1>{landingText.heading}</StyledH1>
+					<StyledH2>{landingText.subHeading}</StyledH2>
+				</TextWrapper>
 
-			<div>
-				<Link href="/contact" as="/contact">
-					<Button variant="contained" style={muiButtonStyle}>
-						contact us
-					</Button>
-				</Link>
-			</div>
-		</Container>
-		// </motion.div>
+				<div>
+					<Link href="/contact" as="/contact">
+						<Button variant="contained" style={muiButtonStyle}>
+							contact us
+						</Button>
+					</Link>
+				</div>
+			</Container>
+		</AnimateWrapper>
 	)
 }
 
